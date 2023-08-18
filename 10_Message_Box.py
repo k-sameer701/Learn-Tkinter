b82a18cbc3dc7eadef9bfa1eefcb4b9aaad4d33c
Sameer Kumar <103239208+k-sameer701@users.noreply.github.com>
2023-08-18 06:37:27 +0530

    Message Box

---
+++ 10_Message_Box.py
@@ -0,0 +1,22 @@
+from tkinter import *
+from tkinter import messagebox
+
+root = Tk()
+
+root.title("Message Box")
+
+# 🔥 DIFFERENT MESSAGE BOXES:
+# 👉 showinfo | showwarning | showerror | askquestion | askokcancel | askyesno 
+
+def popup():
+    response = messagebox.askyesno('This is my Popup','Hello World!')
+    #Label(root, text=response).pack()
+
+    if(response == 1):
+        Label(root, text='You Clicked Yes').pack()
+    else:
+        Label(root, text='You Clicked No').pack()
+
+Button(root, text='Popup', command=popup).pack()
+
+root.mainloop()
