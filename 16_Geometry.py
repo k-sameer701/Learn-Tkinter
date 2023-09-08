0dc3f8ffedb5d02dd31a48372a1f106d9b502d31
Sameer Kumar <103239208+k-sameer701@users.noreply.github.com>
2023-09-08 09:42:29 +0530

    Geometry

---
+++ 16_Geometry.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+root = Tk()
+
+root.title('Geometry')
+
+# 🔥 For Geometry: 'widthxheight'
+root.geometry('644x434')
+
+# 🔥 For Min-size: width, height
+root.minsize(200, 100)
+
+# 🔥 For Max-size: width, height
+root.maxsize(1200, 988)
+
+root.mainloop()
